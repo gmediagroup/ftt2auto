3886f3ec25958f454dda2a6a4cabcb2b89fe10ac
gmediagroup <ravensquad@gmx.com>
2019-03-14 22:39:58 +0200

    Update ftt2.sh

--- ftt2.sh
+++ ftt2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "ftt2 autoinstall"
 wget http://www.ftt2.com/latest/ftt2.zip
 unzip ftt2.zip
 rm ftt2.zip
 chmod -R 0777 ftt2/toplists ftt2/update ftt2/system/logs ftt2/system/templates_c
+echo "done"
